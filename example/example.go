7bcb6c2a3bddcc51483c852d34195e2774ee362c
Andrey Polischuk <me@andrepolischuk.com>
2017-03-03 15:58:55 +0300

    Format sources

--- example/example.go
+++ example/example.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
-  ".."
+	".."
+	"fmt"
+	"math/rand"
 )
 
 func main() {
-  for i := 0; i < 25; i++ {
-    fmt.Println(numd.Decline(rand.Intn(100), "рубль", "рубля", "рублей"))
-  }
+	for i := 0; i < 25; i++ {
+		fmt.Println(numd.Decline(rand.Intn(100), "рубль", "рубля", "рублей"))
+	}
 }
--- numd.go
+++ numd.go
@@ -4,35 +4,34 @@ import "strconv"
 
 // Genetive plural test
 func isGenitivePlural(num int) bool {
-  nn := num % 10
-  return (num > 10 && ((num % 100) - ((num % 100) % 10)) / 10 == 1) ||
-    (nn == 0 || nn >= 5)
+	nn := num % 10
+	return (num > 10 && ((num%100)-((num%100)%10))/10 == 1) || (nn == 0 || nn >= 5)
 }
 
 // Genetive singular test
 func isGenitiveSingular(num int) bool {
-  return num % 10 >= 2
+	return num%10 >= 2
 }
 
 // Numeral variant index
 func numeralIndexOf(num int) int {
-  if num < 0 {
-    num = -num
-  }
-  switch {
-    case isGenitivePlural(num):
-      return 2
-    case isGenitiveSingular(num):
-      return 1
-    default:
-      return 0
-  }
+	if num < 0 {
+		num = -num
+	}
+	switch {
+		case isGenitivePlural(num):
+			return 2
+		case isGenitiveSingular(num):
+			return 1
+		default:
+			return 0
+	}
 }
 
 // Numeral decline
 func Decline(num int, args ...string) string {
-  if len(args) != 3 {
-    return ""
-  }
-  return strconv.Itoa(num) + " " + args[numeralIndexOf(num)]
+	if len(args) != 3 {
+		return ""
+	}
+	return strconv.Itoa(num) + " " + args[numeralIndexOf(num)]
 }
